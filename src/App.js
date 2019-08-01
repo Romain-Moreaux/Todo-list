d4b775e0aad34076af94cea9815f42ceb2a26a54
Romain <6604305+Romain-Moreaux@users.noreply.github.com>
2019-08-01 17:53:18 +0200

    last version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Todo extends React.Component {
+  render() {
+    return (
+      <li className="task">
+        {
+          <React.Fragment>
+            <span
+              className="close"
+              onClick={() => {
+                this.props.removeOnClick();
+              }}
+            />
+            <span
+              className={this.props.done ? "is-done" : ""}
+              onClick={() => {
+                this.props.toggleOnClick();
+              }}
+            >
+              {this.props.title}
+            </span>
+          </React.Fragment>
+        }
+      </li>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    todos: [
+      { title: "Learn to code", done: true },
+      { title: "Rule the world", done: false }
+    ],
+    value: ""
+  };
+
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = event => {
+    let refTodos = [...this.state.todos];
+    refTodos.push({
+      title: this.state.value,
+      done: false
+    });
+    this.setState({ todos: refTodos, value: "" });
+    event.preventDefault();
+  };
+
+  renderList() {
+    return this.state.todos.map((task, index) => {
+      return (
+        <Todo
+          key={index}
+          done={task.done}
+          title={task.title}
+          toggleOnClick={() => {
+            let refTodo = { ...this.state.todos[index] };
+            refTodo.done = !refTodo.done;
+            let refTodos = [...this.state.todos];
+            refTodos[index] = refTodo;
+            this.setState({ todos: refTodos });
+          }}
+          removeOnClick={() => {
+            let refTodos = [...this.state.todos];
+            refTodos.splice(index, 1);
+            this.setState({ todos: refTodos });
+          }}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>To-Do list</h1>
+        {this.state.todos.length > 0 && (
+          <ul className="list"> {this.renderList()}</ul>
+        )}
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            className="input"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="tâche, rappel..."
+          />
+          <button className="submit" type="submit">
+            Valider
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
